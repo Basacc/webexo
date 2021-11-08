5322a66b55c596a4bc6bef595ec520f76684ac45
Basacc <basacc12@gmail.com>
2021-11-08 20:46:49 +0100

    08/11

--- webexo/pendu/index.php
+++ webexo/pendu/index.php
@@ -1,12 +1,31 @@
 <?php
 require "dessin.php";
 $mots = file("mots.txt");
-$toFind = $mots[rand(0,count($mots))];
-$array = [strlen($toFind)];
-for ($i=0;$i<=strlen($toFind);$i++){
-    $array[$i] = "-";
-    echo $array[$i];
+$toFind = str_split(trim($mots[rand(0,count($mots))]));
+$arrayTirets = [strlen(trim($toFind))];
+for ($i=0;$i<strlen($toFind);$i++){
+        $arrayTirets[$i] = "- ";
+        echo "- ";
 }
+$essaisLettres = array();
+$win = false;
+$lost = false;
+while (!$win || !$lost){
+    $input = strtoupper(readline("Entrez un lettre: "));
+    if (!in_array($input, $essaisLettres)){
+        $essaisLettres[] = $input;
+    }
+    else{
+        echo "Essayez un autre lettre";
+        continue;
+    }
+    if (in_array($input, $toFind)){
+        //TODO
+    }
+
+}
+
+
 
 
 
